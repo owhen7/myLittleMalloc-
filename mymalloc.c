9e760a4630c0ac2118e52281b768ddfe2d19473d
Huzaif Mansuri <99559197+HTMansuri@users.noreply.github.com>
2022-02-21 11:54:40 -0500

    Add files via upload
    Please check these files for error and any suggested corrections.

--- mymalloc.c
+++ mymalloc.c
@@ -2,19 +2,80 @@
 #include <stdlib.h>
 #include "mymalloc.h"
 
+void insertAfter(struct header *prev_node, size_t new_size, int old_size)
+{
+    /*1. check if the given prev_node is NULL */
+    /*if (prev_node == NULL)
+    {
+    printf("the given previous node cannot be NULL");
+    return;
+    }*/
 
+    /* 2. allocate new node */
+    struct header* new_node =(struct header*) malloc(sizeof(struct header));
+
+    /* 3. put in the data */
+    new_node->length = (old_size-new_size)-sizeof(struct header);
+    new_node->isFree = 1;
+
+    /* 4. Make next of new node as next of prev_node */
+    new_node->next = prev_node->next;
+
+    /* 5. move the next of prev_node as new_node */
+    prev_node->next = new_node;
+
+    return;
+
+}
 
 static char memory[MEMSIZE];
 
+struct header* startPoint = (struct header*)memory;
+
+startPoint->length = (sizeof(memory) - sizeof(struct header));
+startPoint->isFree = 1;
+startPoint->next = startPoint;
+
 void *mymalloc(size_t size, char *file, int line)
 {
+    //start from the first chunk
+   struct header *current = startPoint;
+
+   //if the first chunk is free
+   /*if(current.isFree) {
+       if((&(current->next)) - (&(current)) >= size)
+        return (&(startPoint) + sizeof(struct header));
+   }*/
+
+   //navigate through complete memory until a free block of required size is found
+   while(!(current->isFree) && (current->length)>=size) {
+
+      //if it is last chunk
+      if(current->next == startPoint) {
+         return "Not Enough Space";
+      } else {
+         //go to next link
+         current = current->next;
+      }
+    }
+
+   //if a valid free chunk is found
+
+
+   //return the address of that chunk for client
+   current->isFree = 0;
+   if((current->length - size)>sizeof(struct header));
+        insertAfter(current, size, (int)current->length);
+   current->length = (int)size;
+   return (&(current))+sizeof(struct header);
+
     printf("Mymalloc was called from %s:%d\n", file, line);
 
     return NULL;
 }
 
+
 void myfree(void *p, char *file, int line)
 {
     printf("free was called from %s:%d\n", file, line);
-
 }
--- mymalloc.h
+++ mymalloc.h
@@ -1,10 +1,20 @@
+#include <stddef.h>
+
 //This code is from the Project1 Writeup. It must be included.
 void *mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
+//void insertAfter(struct header*, size_t, int);
+
+struct header{
+        int length;
+        int isFree;
+        struct header *next;
+    };
 
 //Macro to replace the library functions free() and malloc() with our versions.
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
+#define MEMSIZE 4096
 
 
 //Does anything else go in this header? Or just these lines?
