2d3c6834c145daeffc3f1f0bccc00b872b33101f
owhen7 <osw9@scarletmail.rutgers.edu>
2022-02-28 12:05:04 -0500

    Finished testing, noticed problem
    The 5 test cases are now written. Unfortunately, there is an issue with the myfree function where it seems that it is not truly freeing the metadata associated with the memory that has been allocated. This means I have to write my test cases in a certain way not to expose this. Hopefully we can solve that by today.

--- README.txt
+++ README.txt
@@ -2,35 +2,36 @@ Project by: Owen Wexler and Huzaif Mansuri
 
 Here is the command line we used to run
 
-To compile:gcc -Wall -Werror -fsanitize=address memgrind.c -o memgrind
-To execute:./memgrind.c
+To compile: enter the directory of the project, then type "make" into the command prompt.
+To execute: type "./mymalloc" into the command prompt after compiling.
 
 
-Our Test Plan:
+Our Test Plan: memgrind.c:
 
-
-
-
-How our test programs work, including memgrind.c:
-
-
-memgrind.c:
+Our program runs through five major tests (3 of which have already been given to us), then performs a few mini-tests once each that are designed to show how the program can determine incorrect usage and produce error messages.
 
 Here are the five tests:
 
 1. Allocate and then immediately free the same chunk 120 times, then repeat x50.
 2. Allocate 120 times, then free all 120 chunks in a row, then repeat x50.
 3. Allocate or free randomly with equal chance until we've allocated 120 times total, then free all chunks and repeat x50.
-4.
-5.
+4. Allocate 120 chunks of alternating sizes, then free them in a row, then repeat x50.
+5. Allocate 120 chunks of 1 byte memory, then free only half of them, and reallocate 60 more 2-byte chunks. Then free everything. x50
 
 
-other programs:
 
-We did not write any other test programs. (yet?)
+I DID NOT END UP CODING THESE MINI TESTS IN THE CURRENT STATE OF THE PROGRAM. THEY ARE COMMENTED OUT.
+Here are the Mini-Tests, constructed based on a lecture that Menendez gave on February 23rd.
+1. Allocate all memory, then try to allocate more.
+2. Allocate more memory than exists in total.
+3. Attempt to free an address that is outside the range of MEMSIZE.
+4. Attempt to free an address that isn't the start of a chunk.
+5. Attempt allocate memory when gaps exist, but which are not large enough to fit the memory allocated. (CAN DELETE THIS LAST MINITASK)
+
 
 
 
 
 
 Miscellaneous Notes:
+It was unclear if any additional tests had to be repeated 50 times each.
--- memgrind.c
+++ memgrind.c
@@ -1,106 +1,181 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-//#include "mymalloc.h"
+#include <time.h>
+#include <stdbool.h>
 
-//#include "mymalloc.h" can be commented out to verift this testing program works (memgrind.c) independently of the other files.
-
+#include "mymalloc.h"
+//#define MEMSIZE 4096
 
 //This program is meant to test our malloc and free functions.
 int main(int argc, char**argv)
 {
+	
+    // to store the execution time of code
+    double time_spent[5][50] = {{0.0}};
+    int count = 0;
 
-	//THIS FUNCTION WILL PERFORM EACH TASK 50 TIMES AND RECORD THE TIME IT TAKES FOR EACH TASK.
-  	//NOTE - Error checking for free should happen in the free function itself.
-  	//This program will have minimal output in the final version because it must do each task 50 times.
-  	
-  	
-  	clock_t t; 
-  	t = clock();
-  	
-  	
-  	
-  	//1. malloc and immediately free a 1 byte chunk 120 times.
-  	for(int j = 0; j < 50; j++)
-  	{
-  		for(int i = 0; i < 120; i++)
-		{
-			int *p = malloc(1);
-			free(p);
-  		}
-  	}
+    while(count!=50){
+    clock_t begin = clock();
+
+    //TASK-1 Starts Here
+    //malloc() and immediately free() a 1-byte chunk, 120 times.
+    for(int i=0;i<120;i++){
+        char *p = malloc(1);
+        free(p);
+    }
+
+    clock_t end = clock();
+
+    // calculate elapsed time by finding difference (end - begin) and
+    // dividing the difference by CLOCKS_PER_SEC to convert to seconds
+    time_spent[0][count] += (double)(end - begin) / CLOCKS_PER_SEC;
+
+    begin = clock();
+
+    //TASK-2 Starts Here
+    char *q[120];
+
+    //getting 120 1-byte chunks, and storing the pointers in an array.
+    for(int i=0;i<120;i++){
+        q[i] = malloc(1);
+    }
+
+    //Now Deallocating the chunks
+    for(int i=0;i<120;i++){
+        free(q[i]);
+    }
+
+    end = clock();
+
+    time_spent[1][count] += (double)(end - begin) / CLOCKS_PER_SEC;
+
+    begin = clock();
+
+    //TASK-3 Starts Here
+    int mallocCount = 0;
+    int arrayIndex = 0;
+    char *r[120];
+    while(mallocCount!=120){
+    srand(time(NULL));
+    bool randbool = rand() & 1; //Choose either a zero or a one.
+        if(randbool){
+            r[mallocCount] = malloc(1);
+            arrayIndex++;
+            mallocCount++;
+        }
+        else
+        {
+            if(mallocCount>0)
+            free(r[mallocCount]);
+        }
+    }
+    while(arrayIndex > 0){ //Go back and free all the extra allocations that are left over.
+        free(r[arrayIndex-1]);
+        arrayIndex--;
+    }
+    end = clock();
+
+    time_spent[2][count] += (double)(end - begin) / CLOCKS_PER_SEC;
+
+ 	begin = clock();
+	//TASK-4 Starts here.
+	char *task4[120];
 	
-  	
-  	t = clock()- t;
-  	double time_passed = ((double)t)/CLOCKS_PER_SEC;
-  	printf("Allocate then free 120 times x50 took %f seconds total.\n", time_passed);
-  	
-	t = clock();
-  	//2. malloc() 120 1-byte chunks, store them to an array, and then free all of them afterwards.
-  	for(int j = 0; j < 50; j++)
-  	{
-	  	int *ptr2[120];
-	  	for(int i = 0; i < 120; i++)
-		{
-			ptr2[i] = malloc(1);
-	  	}
-		for(int i = 0; i < 120; i++)
-		{
-			free(ptr2[i]);
-	  	}
-  	}
-  	
-  	t = clock()- t;
-	time_passed = ((double)t)/CLOCKS_PER_SEC;
-  	printf("Allocate 120 times then free 120 times x50 took %f seconds total.\n", time_passed);
-  	
-  	
-  	t = clock();
-	//3. Randomly choose between allocating 1-byte chunk and deallocating a 1-byte chunk.
-	srand((unsigned int)time(NULL)); //It's worth determining if this line goes inside the x50 loop.
-	for(int j = 0; j < 50; j++)
-  	{
-	  	int *ptr3[120]; //This doesn't necessarily need to be 120 spaces long. We are only using a few at the front.
-		int countMalloc = 0;
-		int arrayIndex = 0;
-		while(countMalloc < 120)
-		{
-			int r = rand() % 2; //Generate a random number that is either zero or one.
-			
-			//printf("array index is %d\n", arrayIndex);
-			//use malloc to allocate a byte if the random number is 0
-			if(r == 0)
-			{
-				ptr3[arrayIndex] = malloc(1);
-				arrayIndex++;
-				countMalloc++;
-				
-			}
-			
-			//free a byte our random number is 1 AND we have >0 bytes currently allocated.
-			if(r == 1)
-			{
-				if(arrayIndex > 0) //If we have at least one byte available.
-				{
-					
-					free(ptr3[arrayIndex-1]);
-					arrayIndex--;
-				}
-			}
-		}
-		while(arrayIndex > 0) //Free any remaining allocated chunks.
-		{
-			free(ptr3[arrayIndex-1]);
-			arrayIndex--;
-		}
-  	
-  	}
-  	
-  	t = clock()- t;
-	time_passed = ((double)t)/CLOCKS_PER_SEC;
-	printf("Randomly allocate or free until 120 allocates x50 took %f seconds total.\n", time_passed);
+	for(int i = 0; i < 120; i++)
+	{
+	if(i%2 == 0) //Alternate between 1 sized and 2 sized memory.
+		task4[i] = malloc(1);
+	else
+		task4[i] = malloc(2);
+	}
 	
-	//todo: two more tests of our choice.
+	for(int i = 0; i < 120; i++) //Free all memory.
+	{
+		free(task4[i]);
+	}
+	end = clock();
+	time_spent[3][count] += (double)(end - begin) / CLOCKS_PER_SEC;
 	
-	return EXIT_SUCCESS;
+	
+	//TASK-5 Starts here.
+	
+	begin = clock();
+	char *task5[120];
+	for(int i = 0; i < 120; i++)
+	{
+		task5[i] = malloc(1); //Declare all 120 2-byte chunks.
+	}
+	for(int i = 0; i < 60; i++)
+	{
+		free(task5[i]); //Free chunk 0 through 59. This is half of the chunks we declared.
+	}
+	for(int i = 0; i < 60; i++)
+	{
+		task5[i] = malloc(2); //Declare the first 60 pointers again at size 2. (DOESNT WORK WITH SIZE 5)
+	}
+	for(int i = 0; i < 120; i++) //Free all memory.
+	{
+		free(task5[i]);
+	}
+	
+	end = clock();
+	time_spent[4][count] += (double)(end - begin) / CLOCKS_PER_SEC;
+	
+    count++;
+}
+
+	printf("\n\n%33s","Time (in seconds)");
+	printf("\n--------------------------------------------------");
+	printf("\n  %-11s|  %-12s|  %-12s|  %-12s |  %-12s |  %-12s","Trial #","Task#1","Task#2","Task#3","Task#4","Task#5");
+	printf("\n--------------------------------------------------");
+	
+	for(int j=0;j<50;j++)
+	{
+		printf("\n  %-11d|  %-12.10f  |  %-12.10f  |  %-12.10f | %-12.10f | %-12.10f",j+1,time_spent[0][j],time_spent[1][j],time_spent[2][j], time_spent[3][j], time_spent[4][j]);
+		printf("\n--------------------------------------------------");
+	}
+
+    double sumTaskTime[5]={0.0};
+    double averageTaskTime[5];
+    for(int i=0;i<5;i++){
+        for(int j=1;j<50;j++){
+            sumTaskTime[i] +=  time_spent[i][j];
+        }
+        averageTaskTime[i] = sumTaskTime[i]/50;
+    }
+
+    printf("\n  %-11s|  %-12.10f  |  %-12.10f  |  %-12.10f | %-12.10f | %-12.10f \n","AVERAGE",averageTaskTime[0],averageTaskTime[1],averageTaskTime[2], averageTaskTime[3], averageTaskTime[4]);
+
+	/*
+
+    	printf("MEMSIZE is currently %d.\n", MEMSIZE);
+    	printf("The size of the metadata for each block of memory is currently %ld bytes.\n", sizeof(header));
+	//Feb23 Lecture Mini-Task 1: Allocate all memory, then try to allocate more.
+	printf("\nMini-task 1. Allocating all memory, then try to allocate more...\n");
+	char *miniTask1[2];
+	
+	//Allocate the entire memory, minus space for the metadata.
+	//This should work.
+	miniTask1[0] = malloc(MEMSIZE-32);
+	
+	//Try to allocate more. This fails.
+	miniTask1[1] = malloc(120);
+	
+	free(miniTask1[0]);
+		
+		
+	//Feb23 Lecture Mini-Task 2: Allocate more memory than exists in total.
+	//printf("Mini-task 2. Allocating more memory than exists in total...");
+	//malloc(MEMSIZE + 1); //This fails.
+	
+	//Feb23 Lecture Mini-Task 3: Free an address outside of the range of our memory. 
+	//
+	
+	//Feb23 Lecture Mini-Task 4: Free an address that isn't at the start of a chunk.
+	//
+	
+	//Feb23 Lecture Mini-Task 5: Free every odd pointer, then try and allocate a chunk that is too large for the remaining space.
+	// (CAN DELETE THIS)
+*/
+    return EXIT_SUCCESS;
 }
