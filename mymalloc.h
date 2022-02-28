ce6825d433168e8005199ef6b2dd6368e43edf96
Huzaif Mansuri <99559197+HTMansuri@users.noreply.github.com>
2022-02-28 16:37:03 -0500

    Final mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -2,11 +2,11 @@
 #define MALLOC_H
 #include <stddef.h>
 
-//This code is from the Project1 Writeup. It must be included.
+//Function Prototypes for malloc() and free() functions
 void* mymalloc(size_t size, char *file, int line);
 void myfree(void *ptr, char *file, int line);
-//void insertAfter(struct header*, size_t, int);
 
+//Structure type declaration
 typedef struct header{
         int length;
         int isFree;
@@ -18,7 +18,7 @@ typedef struct header{
 //Macro to replace the library functions free() and malloc() with our versions.
 #define malloc(s) mymalloc(s, __FILE__, __LINE__)
 #define free(p) myfree(p, __FILE__, __LINE__)
+
+//Macro to define the size of our memory array
 #define MEMSIZE 4096
-#define META_SIZE 5
-#endif //MALLOC_H
-//Does anything else go in this header? Or just these lines?
+#endif
